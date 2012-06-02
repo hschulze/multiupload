7187fcacb9b2240b332d56466192122e5ad5b7d7
hschulze <h.schulze.1951@web.de>
2012-06-02 14:07:46 +0200

    Schicker Upload
    (vgl.
    http://webdeveloperplus.com/jquery/ajax-multiple-file-upload-form-using-jquery/
    )

--- index.php
+++ index.php
@@ -2,17 +2,44 @@
 <head>
 <title>Insert title here</title>
 <script src="js/jquery-latest.js" type="text/javascript"></script>
-<script src="js/jquery.MultiFile.js" type="text/javascript"></script>
+<script src="js/ajaxupload.3.5.js" type="text/javascript"></script>
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+<script type="text/javascript" >
+	$(function(){
+		var btnUpload=$('#upload');
+		var status=$('#status');
+		new AjaxUpload(btnUpload, {
+			action: 'upload.php',
+			name: 'uploadfile',
+			onSubmit: function(file, ext){
+				 if (! (ext && /^(jpg|png|jpeg|gif)$/.test(ext))){ 
+                    // extension is not allowed 
+					status.text('Only JPG, PNG or GIF files are allowed');
+					return false;
+				}
+				status.text('Uploading...');
+			},
+			onComplete: function(file, response){
+				//On completion clear the status
+				status.text('');
+				//Add uploaded file to list
+				if(response == "success"){
+					$('<li></li>').appendTo('#files').html('<img src="./uploads/'+file+'" alt="" /><br />'+file).addClass('success');
+				} else{
+					$('<li></li>').appendTo('#files').text(file).addClass('error');
+				}
+			}
+		});
+		
+	});
+</script>
+
 </head>
 <body>
-    <form action="upload.php" method="POST" enctype="multipart/form-data">
-  		Send these files:<br>
-  		<input type="file" name="pic[]" class="multi" />
-  		<input type="submit" name="upload" value="Send files">
-	</form>
-    <?php
+	<div id="upload" ><span>Upload File</span></div><span id="status"></span>
 		
-	?>
-	test
+	<ul id="files" ></ul>
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -1,20 +1,10 @@
 <?php
-	if (isset($_POST['upload'])) { 
-		
-		$uploaddir = 'uploads/';
-		
-		foreach ($_FILES["pic"]["error"] as $key => $error) {
-			if ($error == UPLOAD_ERR_OK) {
-				$tmp_name = $_FILES["pic"]["tmp_name"][$key];
-				$name = $_FILES["pic"]["name"][$key];
-				$uploadfile = $uploaddir . basename($name);
-				
-				if (move_uploaded_file($tmp_name, $uploadfile)) {
-					echo "Success: File " . $name . " uploaded.<br />";
-				} else {
-					echo "Error: File " . $name . " cannot be uploaded.<br />";
-				}
-			}
-		}
-	}
+$uploaddir = './uploads/';
+$file = $uploaddir . basename($_FILES['uploadfile']['name']);
+ 
+if (move_uploaded_file($_FILES['uploadfile']['tmp_name'], $file)) {
+  echo "success";
+} else {
+    echo "error";
+}
 ?>
