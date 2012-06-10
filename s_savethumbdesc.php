054943965cb823190249160a45b5b3edad8692e5
hschulze <h.schulze.1951@web.de>
2012-06-10 20:08:06 +0200

    Serverseitige Implementation des Beschreibungsanpassens

--- s_savethumbdesc.php
+++ s_savethumbdesc.php
@@ -1,7 +1,24 @@
 <?php
 	
 	$thumbid = $_POST['thumbid'];
+	$description = $_POST['description'];
 	
-	echo "$thumbid";
-
+	$db = new mysqli("localhost", "galerieuser", "galerieuser", "galerie");
+	$stmt = $db->stmt_init();
+	if(!$stmt->prepare("UPDATE bilder SET description = ? WHERE id = ?")) {
+		die($db->error);
+	}
+	$stmt->bind_param('si', $description, $thumbid);
+	$stmt->execute();
+	
+	$affected_rows = $stmt->affected_rows;
+	
+	$stmt->close();
+	$db->close();
+	
+	if($affected_rows == 1) {
+		echo json_encode(array("success" => "true"));
+	} else {
+		echo json_encode(array("success" => "false"));
+	}
 ?>
